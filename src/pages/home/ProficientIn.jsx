5ba39871d3ab8c0f429aabbe066a990213973a46
mahbubul-haq <mdmahbubulhaque99@gmail.com>
2024-05-15 01:33:57 +0600

    SkillSection modification for Proficient skills

---
+++ src/pages/home/ProficientIn.jsx
@@ -0,0 +1,197 @@
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
+import { useEffect } from 'react';
+import useTheme from '@mui/material/styles/useTheme';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { proficiencies } from '../../data/data';
+
+const ProficientIn = () => {
+
+    const theme = useTheme();
+    const isMobileScreens = useMediaQuery("(max-width: 600px)");
+    const isNonMobileScreens = useMediaQuery('(min-width: 1000px)');
+    const extraSmallScreens = useMediaQuery('(max-width: 300px)');
+
+    useEffect(() => {
+
+        let observer1 = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                let el = entry.target;
+                if (entry.isIntersecting) {
+                    el.style.opacity = 1;
+                    el.style.transform = "translateY(0) translateX(0)";
+                }
+            });
+        }, {
+            threshold: 0
+        });
+
+        let els1 = document.querySelector(".proficiencies-container");
+        observer1.observe(els1);
+
+        return () => {
+            observer1.unobserve(els1);
+        }
+    }
+        , []);
+
+        const processSkills = (skills) => {
+            if (extraSmallScreens) return skills;
+            if (isMobileScreens) {
+                let result = new Array(skills.length).fill(null), i = 0, j = 0, start = 0;
+                while (j < skills.length && start < 2) {
+                    result[i] = skills[j];
+                    i += 2;
+                    j++;
+                    if (i >= skills.length) {
+                        i = start + 1;
+                        start++;
+                    }
+                }
+                return result;
+            }
+            
+            let result = new Array(skills.length).fill(null), i = 0, j = 0, start = 0;
+            while (j < skills.length && start < 4) {
+                result[i] = skills[j];
+                i += 4;
+                j++;
+                if (i >= skills.length) {
+                    i = start + 1;
+                    start++;
+                } 
+            }
+            return result;
+        }
+
+
+  return (
+    <Box sx={{
+        width: isMobileScreens ? "100%" : "100%",
+        marginTop: isMobileScreens ? "2rem" : isNonMobileScreens ? "2rem" : "2rem",
+        display: isMobileScreens ? "block" : "block",
+        // display: "none",
+        opacity: 1,
+        transform: isMobileScreens ? "translateY(6rem) " : "translateY(6rem) ",
+        transition: isMobileScreens ? "all 0.5s ease-out" : "all 0.5s ease-out",
+
+    }}
+        className="proficiencies-container"
+    >
+        <Typography sx={{
+            fontWeight: 600,
+            fontSize: "1rem",
+            letterSpacing: "0.5px",
+
+        }}>
+            Proficient In
+
+
+        </Typography>
+        <Box sx={{
+            position: "relative",
+            opacity: 0.7
+        }}>
+            <Divider orientation='vertical' sx={{
+                // height: "500px",
+                width: "0.5px",
+                ml: "0.2rem",
+                backgroundColor: theme.palette.primary.light,
+                position: "absolute",
+                top: "0",
+                height: "100%"
+            }} />
+
+
+            {!extraSmallScreens &&
+                <Divider orientation='vertical' sx={{
+                    // height: "500px",
+                    width: "0.5px",
+                    ml: "0.2rem",
+                    mt: "0.5rem",
+                    backgroundColor: theme.palette.primary.light,
+                    position: "absolute",
+                    top: "0",
+                    height: `calc(100% - 0.5rem)`,
+                    left: "50%"
+                }} />
+            }
+            {!isMobileScreens && <>
+
+
+                <Divider orientation='vertical' sx={{
+                    // height: "500px",
+                    width: "0.5px",
+                    ml: "0.2rem",
+                    mt: "0.5rem",
+                    backgroundColor: theme.palette.primary.light,
+                    position: "absolute",
+                    top: "0",
+                    height: `calc(100% - 0.5rem)`,
+                    left: "25%"
+                }} />
+
+                <Divider orientation='vertical' sx={{
+                    // height: "500px",
+                    width: "0.5px",
+                    ml: "0.2rem",
+                    mt: "0.5rem",
+                    backgroundColor: theme.palette.primary.light,
+                    position: "absolute",
+                    top: "0",
+                    height: `calc(100% - 0.5rem)`,
+                    left: "75%"
+                }} />
+            </>}
+            {!extraSmallScreens &&
+                <Divider sx={{
+                    width: !isMobileScreens ? "75%": "50%",
+                    position: "absolute",
+                    backgroundColor: theme.palette.primary.light,
+                    borderBottom: `1px solid ${theme.palette.primary.light}`,
+                    mt: "0.5rem",
+                    ml: "0.2rem",
+                }} />
+            }
+
+            <Box sx={{
+                pt: extraSmallScreens ? "1rem": "2rem",
+                display: "grid",
+                gridTemplateColumns: extraSmallScreens ? "repeat(1, fr)" : isMobileScreens ? "repeat(2, 1fr)" : "repeat(4, 1fr)",
+                // gap: "0.2rem",
+
+            }}>
+                {processSkills(proficiencies).map((skillName, index2) =>
+                    <Box
+                        key={index2}
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+
+                        }}>
+                        <Divider sx={{
+                            // my: "1rem",
+                            width: "2rem",
+                            backgroundColor: theme.palette.primary.light,
+                            ml: "0.2rem",
+                            borderBottom: `1px solid ${theme.palette.primary.light}`
+                        }} />
+                        <Typography sx={{
+                            fontSize: "1rem",
+                            // fontWeight: 600,
+                            letterSpacing: "0.25px",
+
+                            // mb: "1rem",
+                        }}>
+                            {skillName}
+                        </Typography>
+                    </Box>
+                )}
+            </Box>
+        </Box>
+    </Box>
+  )
+}
+
+export default ProficientIn
--- src/pages/home/SkillSection.jsx
+++ src/pages/home/SkillSection.jsx
@@ -1,18 +1,21 @@
-import {useEffect} from 'react'
-import { Box, Divider, Typography } from '@mui/material'
+import { useEffect } from 'react'
+import { Box, Typography } from '@mui/material'
 
-import { skillsData, proficiencies } from '../../data/data'
+import { skillsData } from '../../data/data'
 
 import useMediaQuery from '@mui/material/useMediaQuery'
 import useTheme from '@mui/material/styles/useTheme'
 import icons from '../../data/skillicons'
-
+import ProficientIn from './ProficientIn'
+import sample from "../../assets/images/sample.svg"
 
 
 const SkillSection = () => {
 
     const isMobileScreens = useMediaQuery("(max-width: 600px)");
     const isNonMobileScreens = useMediaQuery('(min-width: 1000px)');
+    const extraLargeScreens = useMediaQuery('(min-width: 1300px)');
+
     const theme = useTheme();
 
     useEffect(() => {
@@ -21,7 +24,7 @@ const SkillSection = () => {
             entries.forEach(entry => {
                 let el = entry.target;
                 if (entry.isIntersecting) {
-                    el.style.opacity = 1;
+                    el.style.opacity = 0.6;
                     el.style.transform = "translateY(0) translateX(0)";
                 }
             });
@@ -47,18 +50,37 @@ const SkillSection = () => {
             observer.observe(el);
         });
 
-        let els1 = document.querySelector(".proficiencies-container");
-        observer1.observe(els1);
+        let el1 = document.querySelector(".skills-fill-gap-image");
+        if (el1) observer1.observe(el1);
+
+        // let event = window.addEventListener('resize', () => {
+        //     let el = document.querySelector(".skills-fill-gap-image");
+            
+        //     if (window.innerWidth >= 1300) {
+        //         if (el) el.style.display = "flex";
+        //     }
+        //     else {
+        //         if (el) el.style.display = "none";
+        //     }
+        // });
 
         return () => {
             els.childNodes.forEach(el => {
                 observer.unobserve(el);
             });
 
-            observer1.unobserve(els1);
-        }   
+            if (el1) observer1.unobserve(el1);
+            // window.removeEventListener('resize', event);
+        }
 
     }, []);
+
+    // useEffect(() => {
+    //     if (extraLargeScreens) {
+    //         let el = document.querySelector(".skills-fill-gap-image").style.display = "flex";
+    //     }
+    // }, [extraLargeScreens, isMobileScreens, isNonMobileScreens]);
+
 
     return (
         <Box sx={{
@@ -74,146 +96,99 @@ const SkillSection = () => {
             <Box sx={{
                 display: "flex",
                 justifyContent: "space-between",
-                flexDirection: isMobileScreens ? "column" : "row",
+                flexDirection: isMobileScreens ? "column" : "column",
                 gap: "1rem",
 
+            }}> <Box sx={{
+                display: "flex",
+                gap: "3rem",
             }}>
-                <Box
-                    className="skills-container"
-                 sx={{
-                    display: "flex", flexDirection: "column", gap: "3rem",
-                    width: isMobileScreens ? "100%" : "80%",
-                }}>
-
-                    {skillsData.map((skill, index) => (
-
-                        <Box key={skill.skillType + index}
-                            className={skill.skillType}
-                            sx={{
-                                opacity: 0,
-                                transform: "translateY(4rem)",
-                                transition: "all 0.5s ease-out",
-                            }}
-                        >
-                            <Typography sx={{
-                                fontWeight: 600,
-                                fontSize: "1rem",
-                                letterSpacing: "0.5px",
-                                mb: "1rem",
-                            }}>
-                                {skill.skillType}
-                            </Typography>
-                            <Box sx={{
-                                display: "flex",
-                                flexWrap: "wrap",
-                                gap: isMobileScreens ? "0.5rem" : "0.8rem",
-                            }}>
-                                {skill.list.map((item, index1) => (
-                                    <Box key={index1} sx={{
-                                        display: "flex",
-                                        gap: "0.6rem",
-                                        alignItems: "center",
-                                        p: isMobileScreens ? "0.5rem 0.7rem" : "0.7rem 1rem",
-                                        backgroundColor: "rgba(0,0,0,0.1)",
-                                        width: "fit-content",
-                                        borderRadius: "0.3rem",
-                                        background: theme.palette.background.skillChip,
-                                    }}>
-                                        {!isMobileScreens && <Box sx={{
-                                            color: icons[item.name]?.color,
-                                            fontSize: "1.5rem",
-
-                                            display: "flex",
-                                            alignItems: "center",
-                                        }}>
-                                            {icons[item.name]?.icon}
-                                        </Box>}
-                                        <Typography sx={{
-                                            fontSize: "1rem",
-                                            opacity: 0.9,
-                                        }}>
-                                            {item.name}
-                                        </Typography>
-                                    </Box>
-                                ))}
-
-                            </Box>
-                        </Box>
-
-                    ))}
-
-                </Box>
-                
-                <Box sx={{
-                    width: isMobileScreens ? "100%" : "20%",
-                    marginTop: isMobileScreens ? "2rem" : isNonMobileScreens ? "-6rem" : "-4rem",
-                    display: isMobileScreens ? "block" : "block",
-                    opacity: 0,
-                    transform: isMobileScreens ? "translateY(6rem)" : "translateX(4rem)",
-                    transition: isMobileScreens ? "all 0.5s ease-out" : "all 0.5s ease-out 0.5s",
-                }}
-                className="proficiencies-container"
-                >
-                    <Typography sx={{
-                        fontWeight: 600,
-                        fontSize: "1rem",
-                        letterSpacing: "0.5px",
-
-                    }}>
-                        Proficient In
-
-
-                    </Typography>
-                    <Box sx={{
-                        position: "relative",
-                        opacity: 0.7
-                    }}>
-                        <Divider orientation='vertical' sx={{
-                            // height: "500px",
-                            width: "0.5px",
-                            ml: "0.2rem",
-                            backgroundColor: theme.palette.primary.light,
-                            position: "absolute",
-                            top: "0",
-                            height: "100%"
-                        }} />
-
-                        <Box sx={{
-                            pt: "1.5rem",
-                            display: "flex",
-                            flexDirection: "column",
-                            gap: "0.2rem",
-                        
+                    <Box
+                        className="skills-container"
+                        sx={{
+                            display: "flex", flexDirection: "column", gap: "3rem",
+                            width: extraLargeScreens ? "60%" : isNonMobileScreens ? "100%" : "100%",
+                            
                         }}>
-                            {proficiencies.map((skillName, index2) => 
-                            <Box
-                                key={index2}
-                             sx={{
-                                display: "flex",
-                                alignItems: "center",
-                               
-                            }}>
-                                <Divider sx={{
-                                    // my: "1rem",
-                                    width: "2rem",
-                                    backgroundColor: theme.palette.primary.light,
-                                    ml: "0.2rem",
-                                    borderBottom: `1px solid ${theme.palette.primary.light}`
-                                }} />
+
+                        {skillsData.map((skill, index) => (
+
+                            <Box key={skill.skillType + index}
+                                className={skill.skillType}
+                                sx={{
+                                    opacity: 0,
+                                    transform: "translateY(4rem)",
+                                    transition: "all 0.5s ease-out",
+                                }}
+                            >
                                 <Typography sx={{
+                                    fontWeight: 600,
                                     fontSize: "1rem",
-                                    // fontWeight: 600,
-                                    letterSpacing: "0.25px",
-                                    
-                                    // mb: "1rem",
+                                    letterSpacing: "0.5px",
+                                    mb: "1rem",
                                 }}>
-                                    {skillName}
+                                    {skill.skillType}
                                 </Typography>
+                                <Box sx={{
+                                    display: "flex",
+                                    flexWrap: "wrap",
+                                    gap: isMobileScreens ? "0.5rem" : "0.8rem",
+                                }}>
+                                    {skill.list.map((item, index1) => (
+                                        <Box key={index1} sx={{
+                                            display: "flex",
+                                            gap: "0.6rem",
+                                            alignItems: "center",
+                                            p: isMobileScreens ? "0.5rem 0.7rem" : "0.7rem 1rem",
+                                            backgroundColor: "rgba(0,0,0,0.1)",
+                                            width: "fit-content",
+                                            borderRadius: "0.3rem",
+                                            background: theme.palette.background.skillChip,
+                                        }}>
+                                            {!isMobileScreens && <Box sx={{
+                                                color: icons[item.name]?.color,
+                                                fontSize: "1.5rem",
+
+                                                display: "flex",
+                                                alignItems: "center",
+                                            }}>
+                                                {icons[item.name]?.icon}
+                                            </Box>}
+                                            <Typography sx={{
+                                                fontSize: "1rem",
+                                                opacity: 0.9,
+                                            }}>
+                                                {item.name}
+                                            </Typography>
+                                        </Box>
+                                    ))}
+
+                                </Box>
                             </Box>
-                            )}
-                        </Box>
+
+                        ))}
+
                     </Box>
+                    {/* {extraLargeScreens && */}
+                        <Box sx={{
+                            width: "40%",
+                            // display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            opacity: 0,
+                            transform: "translateX(4rem)",
+                            transition: "all 0.5s ease-out",
+                            display: extraLargeScreens ? "flex" : "none",
+                        }}
+                        className="skills-fill-gap-image"
+                        >
+
+                            <img src={sample} alt="sample" style={{ width: "100%", height: "auto" }} />
+                        </Box>
+                    {/* } */}
                 </Box>
+
+                <ProficientIn />
             </Box>
 
         </Box>
